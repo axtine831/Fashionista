cf2a2d268c0f0cdb4dacec521921c5c0edfbfb84
axtine831 <ampstine@gmail.com>
2012-09-22 15:59:03 +0300

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fashionista::Application.config.secret_token = '475d1d7193fa5dd2250b4714ec78b80aa8c5b43676813b33ba0da3543e4780ff4d69d2356ccc455e91509456d7ebb6e4209105d55a4c813c24f6ba7e5464c6dd'
